3d42b7b701be3b1f786f1f80a5b7388875865da7
kidsana panya <kidsana.p@gmail.com>
2018-07-24 14:45:04 +0700

    update template

--- application/modules/tax_estimate/views/index.php
+++ application/modules/tax_estimate/views/index.php
@@ -9,29 +9,54 @@
      <div class="x_panel">
      <br>
      
-     <form method="post" action="<?php echo base_url('Tax_estimate/saveEstimate') ?>">
+     <form method="post" action="<?php echo base_url('tax_estimate/saveEstimate') ?>">
           <table class="table table-bordered jambo_table">
               <thead>
                 <tr>
                   <th width="80%">หมวดรายได้</th>
-                  <th>ประมาการ</th>
+                  <th class="text-center">ประมาณการ</th>
                 </tr>
               </thead>
               <tbody>
-              <?php foreach ($tax[0] as $key => $value) { ?>
+              <?php 
+              
+              $sum_all = 0;
+
+              foreach ($tax[0] as $key => $value) { 
+                  $id = 'c'.$key;
+                ?>
                 <tr>
                   <td><span style="font-weight: bolder;"><?php echo $value->tax_name; ?></span></td>
-                  <td></td>
+                  <td id="<?php echo $id;?>" class="numeric text-right" style="font-size: 16px;font-weight: bold"></td>
                 </tr>
-                <?php foreach ($tax[$value->tax_id] as $key => $value2) { ?>
+                <?php 
+                $sum_p1 = 0; 
+
+                foreach ($tax[$value->tax_id] as $key2 => $value2) { ?>
                   <tr>
                     <td><span style="padding-left: 15px;"><?php echo $value2->tax_name; ?></span></td>
-                    <td><input type="text" class="numeric text-right form-control" name="estimate_tax[<?php echo $value2->tax_id ?>]" value="<?php echo $value2->tax_estimate ?>" ></td>
+                    <td><input onkeyup="cal('<?php echo $id;?>')" type="text" class="<?php echo $id;?> numeric text-right form-control" name="estimate_tax[<?php echo $value2->tax_id ?>]" value="<?php echo $value2->tax_estimate ?>" ></td>
                   </tr>
                 <?php 
+                  $sum_p1+=$value2->tax_estimate;
               } ?>
               <?php 
+
+              if($sum_p1>0) {
+                $sum_all+=$sum_p1; 
+              ?>
+                <script>$("#c<?php echo $key;?>").html("<?php echo number_format($sum_p1,2);?>");</script>
+              <?php
+              }
+
+
             } ?>
+
+                <tr>
+                  <td><span style="padding-left: 15px; font-size: 18px; font-weight: bold">รวม</span></td>
+                  <td id="sum_all" class="text-right numeric" style="font-size: 18px;font-weight: bold"><?php echo number_format($sum_all,2);?></td>
+                </tr>
+
               </tbody>
             </table>
 
@@ -50,5 +75,37 @@
   </div>
 </div>
 
+
+<script>
+function addCommas(n)
+{
+  var val = Math.round(Number(n) *100) / 100;
+  var parts = val.toString().split(".");
+  
+  if(parts[1]<10)parts[1]=parts[1]+'0';
+
+  var num = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : ".00");
+  
+  return num;
+}
+
+  function cal(node) {
+    //console.log($("#"+$(node).data("node")).html());
+    console.log(node);
+
+    var sum = 0;
+    $('input.'+node).each(function(index,data){
+        sum = sum+parseFloat($(data).val());
+    });
+    $('#'+node).html(addCommas(sum));
+
+    sum_all = 0;
+    $("input.numeric").each(function(index,data){
+        sum_all = sum_all+parseFloat($(data).val());
+    });
+    $("#sum_all").html(addCommas(sum_all));
+
+  }
+</script>
 
 
